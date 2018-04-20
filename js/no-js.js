9c8cc9ff791838bdae374cb1604df303783ebb1b
inneska-v <36503787+inneska-v@users.noreply.github.com>
2018-04-20 19:05:36 +0300

    Add files via upload
    Работаем с JS
    
    1. Подключен no-js, sticky-header.
    2. Для реализации sticky-header были внесены изменения в html .header_content,  и в css - добавлен .page_header-sticky.

---
+++ js/no-js.js
@@ -0,0 +1,14 @@
+(function() {
+  
+  var doc = document.querySelector("body");
+  if (doc.classList.contains("no-js")) {
+      
+      doc.classList.remove("no-js");
+      
+      } else {
+      
+      return;
+      
+      }
+  
+})();
