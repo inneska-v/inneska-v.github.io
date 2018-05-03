773b28d091931a6202432404c46b4b526890f7dd
inneska-v <36503787+inneska-v@users.noreply.github.com>
2018-05-03 15:21:24 +0300

    Add files via upload
    Подключили форму обратной связи на JS

---
+++ js/contactform.js
@@ -0,0 +1,22 @@
+jQuery(document).ready(function($) {
+
+$(".ajax-contact-form").submit(function() {
+var th = $(this);
+
+$.ajax({
+type: "POST",
+url: "http://github.com/inneska-v/mail.php",
+data: th.serialize()
+}).done (function (){
+	$(th).find('.success').addClass('.active').css('display','flex').hide().fadeIn();
+
+	setTimeout (function() {
+		$(th).find('.success').removeClass('.active').fadeOut();
+		th.trigger('reset');
+	}, 3000);
+});
+
+return false;
+
+});
+});
