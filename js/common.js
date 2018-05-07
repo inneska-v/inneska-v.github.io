ba31a2e0a23a7ed8af964be1e3e6f7d78f47938a
inneska-v <36503787+inneska-v@users.noreply.github.com>
2018-05-07 12:36:56 +0300

    Add files via upload
    Добавлена интерактивность на странице с помощью animate.css на js

---
+++ js/common.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+	//Example: $(".element").animated("zoomInUp");
+
+$("h1, h2, h3").animated("fadeInDown", "fadeOutDown");
+$("p").animated("flipInX", "fadeOutDown");
+$("strong").animated("zoomIn", "fadeOutDown");
+
+$(".news_area .news_wrap-first").animated("fadeInLeft", "fadeOutDown");
+$(".news_area .first_img").animated("fadeInRight", "fadeOutDown");
+$(".news_area .second_img").animated("fadeInLeft", "fadeOutDown");
+$(".news_area .news_wrap-second").animated("fadeInRight", "fadeOutDown");
+
+$(".blog .blog_img").animated("flipInY", "fadeOutDown");
+
+
+$(".advantages .advantages_wrap").animated("fadeInLeft", "fadeOutDown");
+$(".advantages .advantages_list").animated("fadeInLeft", "fadeOutDown");
+$(".advantages .advantages_img").animated("fadeInRight", "fadeOutDown");
+
+$(".team .workers").animated("flipInY", "fadeOutDown");
+$(".team .workers_block").animated("zoomIn", "fadeOutDown");
+	
+
+$(".footer .container_map").animated("rotateIn", "fadeOutDown");
+
+
+})
